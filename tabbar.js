98a00c374fc9e912f6d825439a4fa141bea6eedc
Shpat Gjoshi <gjoshishpat@gmail.com>
2026-01-26 15:38:37 +0100

    certificates added

--- tabbar.js
+++ tabbar.js
@@ -4,14 +4,14 @@
         const Contact = document.querySelector('.Contact');
 
           home.addEventListener('click', () => {
-            if(window.location.href === "http://localhost") return
-            window.location.replace("http://localhost");
+            if(window.location.href === "https://gjoshi99.github.io/Portfolio/") return
+            window.location.replace("https://gjoshi99.github.io/Portfolio/");
         })
 
         Contact.addEventListener('click', () => {
             console.log("Contact", window.location.href)
-            if(window.location.href === "http://localhost/Contact") return
-            window.location.assign('Contact.html');
+            if(window.location.href === "https://gjoshi99.github.io/Portfolio/Contact") return
+            window.location.assign('Contact');
         })
 
 
